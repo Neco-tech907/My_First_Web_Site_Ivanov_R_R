40770ab9a67a06bc2f81f93f2c5090f9967d34ae
Raul <140114218+Neco-tech907@users.noreply.github.com>
2025-03-28 12:57:34 +0300

    test style2 + language correct

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Иванов Р.Р.</title>
+    <title>Ivanov R.R.</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6C01i6uLrA9TneNEoa7RxnatzjcDSCmG1WXxSR16AaXEV/DwwykczNPK8W2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icon@d1.5.0/font/bootstrap-icons.css"/>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style2.css">
 </head>
 
 <body>
@@ -21,7 +21,7 @@
     <a href="/registration.php">Зарегистрируйтесь</a> или <a href="/login.php">войдите</a>!
     <?php
     } else {
-    // подключение к БД
+    // connetc to server
     }
     ?>
     </div>
