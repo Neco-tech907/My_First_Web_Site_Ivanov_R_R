62fc17d2d576e91bb765f02629bf813e127d1898
Raul <140114218+Neco-tech907@users.noreply.github.com>
2025-03-28 13:16:28 +0300

    форматирование + style2.css

--- index.php
+++ index.php
@@ -1,30 +1,24 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ivanov R.R.</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6C01i6uLrA9TneNEoa7RxnatzjcDSCmG1WXxSR16AaXEV/DwwykczNPK8W2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icon@d1.5.0/font/bootstrap-icons.css"/>
+        integrity="sha384-T3c6C01i6uLrA9TneNEoa7RxnatzjcDSCmG1WXxSR16AaXEV/DwwykczNPK8W2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icon@d1.5.0/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="css/style2.css">
 </head>
 
 <body>
-<div class="container">
-    <div class="row">
-    <div class="col-12 index">
-    <h1>Авторизируйтесь!</h1>
-    <?php
-    if (!isset($_COOKIE['User'])) {
-    ?>
-    <a href="/registration.php">Зарегистрируйтесь</a> или <a href="/login.php">войдите</a>!
-    <?php
-    } else {
-    // connetc to server
-    }
-    ?>
+    <div class="auth-container">
+        <h1 class="auth-title">Авторизируйтесь!</h1>
+        <div class="auth-links">
+            <a href="/registration.php" class="auth-link">Зарегистрируйтесь</a>
+            <span class="auth-link-divider">|</span>
+            <a href="/login.php" class="auth-link">Войти</a>
+        </div>
     </div>
-    </div>
-    </body>
-    </html>
+
+</html>
--- js/button.js
+++ js/button.js
@@ -1,22 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var count = 0;
-    var button = document.getElementById("toggle-button");
-    var container = document.getElementById("image-container");
-    
-    button.onclick = function() {
-        count++;
-        
-        if (count % 2 == 0) {
-            // Четное нажатие - скрываем изображение
-            container.innerHTML = "";
-            button.textContent = "Показать фото";
-        } else {
-            // Нечетное нажатие - показываем изображение
-            var img = document.createElement("img");
-            img.src = "images/img3.png";
-            img.alt = "Фото Галимова А.Д.";
-            container.appendChild(img);
-            button.textContent = "Скрыть фото";
-        }
-    };
+document.addEventListener("DOMContentLoaded", function () {
+  var count = 0;
+  var button = document.getElementById("toggle-button");
+  var container = document.getElementById("image-container");
+
+  button.onclick = function () {
+    count++;
+
+    if (count % 2 == 0) {
+      // Четное нажатие - скрываем изображение
+      container.innerHTML = "";
+      button.textContent = "Показать фото";
+    } else {
+      // Нечетное нажатие - показываем изображение
+      var img = document.createElement("img");
+      img.src = "images/img3.png";
+      img.alt = "Фото Галимова А.Д.";
+      container.appendChild(img);
+      button.textContent = "Скрыть фото";
+    }
+  };
 });
