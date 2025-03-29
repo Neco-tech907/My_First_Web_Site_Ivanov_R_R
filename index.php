d3dd413f2d4fd1f5284fe8578d9466f1c65a2e34
Raul <140114218+Neco-tech907@users.noreply.github.com>
2025-03-29 12:51:53 +0300

    posts + index.php logic

--- index.php
+++ index.php
@@ -7,18 +7,31 @@
     <title>Ivanov R.R.</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6C01i6uLrA9TneNEoa7RxnatzjcDSCmG1WXxSR16AaXEV/DwwykczNPK8W2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icon@d1.5.0/font/bootstrap-icons.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icon@1.5.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/style2.css">
 </head>
 
 <body>
     <div class="auth-container">
         <h1 class="auth-title">Авторизируйтесь!</h1>
-        <div class="auth-links">
-            <a href="registration.php" class="auth-link">Зарегистрируйтесь</a>
-            <span class="auth-link-divider">|</span>
-            <a href="login.php" class="auth-link">Войти</a>
-        </div>
+        <?php if (!isset($_COOKIE['User'])): ?>
+            <div class="auth-links">
+                <a href="registration.php" class="auth-link">Зарегистрируйтесь</a>
+                <span class="auth-link-divider">|</span>
+                <a href="login.php" class="auth-link">Войти</a>
+            </div>
+        <?php else:
+            $link = mysqli_connect('127.0.0.1', 'root', 'kali', 'db_kali');
+            $sql = "SELECT * FROM posts";
+            $res = mysqli_query($link, $sql);
+            if (mysqli_num_rows($res) >  0) {
+                while ($post = mysqli_fetch_array($res)) {
+                    echo "<a href='/posts.php?id=" . $post["id"] . "'>" . $post['title'] . "</a>\n";
+                }
+            } else {
+                echo "Записей пока нет";
+            }
+        ?>
     </div>
 </body>
 
